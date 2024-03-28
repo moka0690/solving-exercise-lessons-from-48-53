9a2b479011f25ae95c6620c8d22775b72cc5acf9
moka0690 <madradz39@gmail.com>
2024-03-28 21:58:18 +0100

    solveSolving exercise 1

---
+++ main.js
@@ -0,0 +1,28 @@
+let start = 10;
+let end = 100;
+let exclude = 40;
+
+for(let i=start; i<end+start ;i=i+10){
+    if(i===exclude){
+        continue
+    }
+    console.log (i)
+}
+
+
+
+
+
+
+
+
+// Output
+10
+20
+30
+50
+60
+70
+80
+90
+100
