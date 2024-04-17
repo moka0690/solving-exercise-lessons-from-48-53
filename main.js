4bd97904d30cab8df1cb2ecfec931b3cc0c9d5c1
moka0690 <madradz39@gmail.com>
2024-04-17 23:28:37 +0100

    Exercise solution  7

--- main.js
+++ main.js
@@ -116,3 +116,23 @@ for( i=friends.indexOf("Ahmed");i<newFriends.length;i++){
 // "3 => Mahmoud"
 // "4 => Osama"
 // "5 => Sameh"
+
+
+let start = 0;
+let swappedName = "elZerO";
+let newSwappedName ="";
+
+
+// Output
+"ELzERo"
+for  (let i=start;i<swappedName.length;i++)
+    if(swappedName[i]===swappedName[i].toUpperCase()){
+        newSwappedName = newSwappedName+swappedName[i].toLowerCase()
+   }else{
+       newSwappedName = newSwappedName+swappedName[i].toUpperCase()
+    }
+console.log(newSwappedName)
+
+
+
+
